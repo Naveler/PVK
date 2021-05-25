b580863fa5247d5e90cba578aa477772c08ae997
Naveler <georg.vilja@khk.ee>
2021-05-25 15:08:17 +0300

    Loodud ja võetud kasutusele vaade failid

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -9,16 +9,21 @@ class Pages extends Controller
      */
     public function __construct()
     {
-        echo 'page class is loaded<br>';
+        // echo 'page class is loaded<br>';
     }
 
     public function index(){
-        echo 'index mehod is loaded<br>';
-        $this->view('index');
+        // echo 'index mehod is loaded<br>';
+        $data = array(
+            'title' => 'Welcome to Pages by mvc',
+            'content' => 'Pages index view is loaded by mvc'
+        );
+        $this->view('pages/index', $data);
     }
 
     public function about(){
-        echo 'about mehod is loaded<br>';
-        $this->view('about');
+        // echo 'about mehod is loaded<br>';
+        $data = array('content' => 'Pages about view is loaded by MVC');
+        $this->view('pages/about', $data);
     }
 }
