5ca362d1081c16c9941e3b3dd494027c47a2d4a0
Naveler <georg.vilja@khk.ee>
2021-05-25 15:54:49 +0300

    Lisatud kastutusele vaade päis ja jalus

--- app/config/constants.php
+++ app/config/constants.php
@@ -4,4 +4,4 @@ define('PROJECT_NAME', 'pvk');
 // App root dir
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL root
-define ('URLROOT', 'http//'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
+define ('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,3 +1,3 @@
-<p>
-    <?php echo $data['content'];?>
-</p>
+<?php require_once APPROOT.'/views/inc/header.php'; ?>
+<p><?php echo $data['content'];?></p>
+<?php require_once APPROOT.'/views/inc/footer.php'; ?>
